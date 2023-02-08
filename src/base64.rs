5a4e81912fefa52dfca96fc62646fb44f086a690
Insopitus <jessefor@hotmail.com>
2023-02-09 01:07:32 +0800

    base64 encode padding

--- src/base64.rs
+++ src/base64.rs
@@ -12,12 +12,35 @@ pub fn base64_encode(buffer: &[u8]) -> String {
     let out_len = (in_len + 2) / 3 * 4;
     let mut output = String::with_capacity(out_len);
 
-    for i in 0..in_len / 3 {
-        encode_slice(&buffer[i*3..=i*3+2], &mut output);
+    let slices = in_len / 3;
+
+    for i in 0..slices {
+        encode_slice(&buffer[i * 3..=i * 3 + 2], &mut output);
     }
     // padding
-    if in_len % 3 != 0 {
-        let padding_in:[u8;4] = [buffer[in_len]];
+    let remains = in_len % 3;
+    match remains {
+        0 => {}
+        1 => {
+            let a = buffer[slices * 3];
+            let char1 = a >> 2;
+            let char2 = a << 6;
+            output.push(TABLE[char1 as usize]);
+            output.push(TABLE[char2 as usize]);
+            output.push_str("==");
+        }
+        2 => {
+            let a = buffer[slices * 3];
+            let b = buffer[slices * 3 + 1];
+            let char1 = a >> 2;
+            let char2 = (a << 4 | b >> 4) & 0b00111111;
+            let char3 = b << 2 & 0b00111111;
+            output.push(TABLE[char1 as usize]);
+            output.push(TABLE[char2 as usize]);
+            output.push(TABLE[char3 as usize]);
+            output.push('=');
+        }
+        _ => unreachable!(),
     }
     assert_eq!(out_len, output.len());
 
@@ -29,19 +52,19 @@ pub fn base64_decode(s: &str) -> Vec<u8> {
 }
 
 /// panic if slice length isn't 3
-fn encode_slice(slice:&[u8],str:&mut String){
+fn encode_slice(slice: &[u8], str: &mut String) {
     let a = slice[0];
-        let b = slice[1];
-        let c = slice[2];
-        let char1 = a >> 2;
-        let char2 = (a << 4 | b >> 4) & 0b00111111;
-        let char3 = (b << 2 | c >> 6) & 0b00111111;
-        let char4 = c & 0b00111111;
-        // TODO use ascii to prevent type casting or raw pointer?
-        str.push(TABLE[char1 as usize]);
-        str.push(TABLE[char2 as usize]);
-        str.push(TABLE[char3 as usize]);
-        str.push(TABLE[char4 as usize]);
+    let b = slice[1];
+    let c = slice[2];
+    let char1 = a >> 2;
+    let char2 = (a << 4 | b >> 4) & 0b00111111;
+    let char3 = (b << 2 | c >> 6) & 0b00111111;
+    let char4 = c & 0b00111111;
+    // TODO use ascii to prevent type casting or raw pointer?
+    str.push(TABLE[char1 as usize]);
+    str.push(TABLE[char2 as usize]);
+    str.push(TABLE[char3 as usize]);
+    str.push(TABLE[char4 as usize]);
 }
 
 #[cfg(test)]
@@ -56,5 +79,6 @@ mod test {
     fn padding() {
         assert_eq!(base64_encode(b"abcd"), "YWJjZA==");
         assert_eq!(base64_encode(b"abcde"), "YWJjZGU=");
+        assert_eq!(base64_encode(b"sageskjkbvnmiksjgtkgeskjgkgesGEKSAGNSGMSJKGKMVLKSJKGNKSNGLAJLKGHKSNKBAL;AJKKLGHSKNGALJHKNBZ.MOSGM.A.[91328I"),"c2FnZXNramtidm5taWtzamd0a2dlc2tqZ2tnZXNHRUtTQUdOU0dNU0pLR0tNVkxLU0pLR05LU05HTEFKTEtHSEtTTktCQUw7QUpLS0xHSFNLTkdBTEpIS05CWi5NT1NHTS5BLls5MTMyOEk=")
     }
 }
