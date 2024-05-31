46cfb71543fc3faaaf3f09925da3679ac2ecc659
insopitus <jessefor@hotmail.com>
2024-06-01 00:35:17 +0800

    add unit test

--- src/interpolate/lerp.rs
+++ src/interpolate/lerp.rs
@@ -13,6 +13,8 @@ where
 
 #[cfg(test)]
 mod test{
+    use crate::linear_algebra::Vector3;
+
     use super::*;
     #[test]
     fn lerp_numbers(){
@@ -20,5 +22,11 @@ mod test{
         assert_eq!(lerp(1.0,2.0,0.5),1.5);
         assert_eq!(lerp(2.0,0.0,0.25),1.5);
 
+    }
+    #[test]
+    fn lerp_vector3(){
+        let v1 = Vector3::new(1.0, 1.0, 1.0);
+        let v2 = Vector3::new(0.0, 0.0, 0.0);
+        assert_eq!(lerp(v1, v2, 0.5),Vector3::new(0.5, 0.5, 0.5));
     }
 }
